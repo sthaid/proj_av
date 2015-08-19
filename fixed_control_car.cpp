23e5f210349e7facd683fd1b679c638146305823
Steven Haid <stevenhaid@gmail.com>
2015-08-19 18:49:29 -0400

    simplify fixed_control_car.cpp

--- fixed_control_car.cpp
+++ fixed_control_car.cpp
@@ -1,5 +1,3 @@
-#include <math.h>
-
 #include "fixed_control_car.h"
 #include "logging.h"
 #include "utils.h"
@@ -15,13 +13,9 @@ fixed_control_car::~fixed_control_car()
 
 void fixed_control_car::update_controls(double microsecs)
 {
+    // get front_view, this is done to just get an idea of the processing time
     unsigned char front_view[200*200];
     world &w = get_world();
-
-    // get front_view
     w.get_view(get_x(), get_y(), get_dir(), 200, 200, reinterpret_cast<unsigned char *>(front_view));
-
-    //set_steer_ctl(get_dir() < 90 ? 10 : 0);
-    //set_speed_ctl(get_speed() < 20 ? 10 : 0);
 }
 

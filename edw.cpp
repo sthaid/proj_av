67d16114ebcb4d18f595f0962e24f634d2274b43
Steven Haid <stevenhaid@gmail.com>
2015-08-19 18:51:34 -0400

    edw.cpp cleanup

--- edw.cpp
+++ edw.cpp
@@ -1,11 +1,10 @@
 // XXX 
 // - add edit pixels
-// - on clear, reset cursor
-// - use common.h for some of these globals (maybe)
 
 #include <sstream>
 #include <unistd.h>
 #include <string.h>
+#include <math.h>
 
 #include "display.h"
 #include "world.h"
@@ -13,24 +12,53 @@
 #include "logging.h"
 #include "utils.h"
 
-// XXX make this similar to av.cpp
+// display and pane size
+#define DISPLAY_WIDTH        1420
+#define DISPLAY_HEIGHT       800
 
+#define PANE_WORLD_ID        0
+#define PANE_WORLD_X         0 
+#define PANE_WORLD_Y         0
 #define PANE_WORLD_WIDTH     800
 #define PANE_WORLD_HEIGHT    800
+
+#define PANE_CTRL_ID         1
+#define PANE_CTRL_X          820
+#define PANE_CTRL_Y          0
 #define PANE_CTRL_WIDTH      600
-#define PANE_CTRL_HEIGHT     800
-#define PANE_SEPERATION      20
+#define PANE_CTRL_HEIGHT     750
 
-#define DISPLAY_WIDTH   (PANE_WORLD_WIDTH + PANE_SEPERATION + PANE_CTRL_WIDTH)
-#define DISPLAY_HEIGHT  (PANE_WORLD_HEIGHT >= PANE_CTRL_HEIGHT ? PANE_WORLD_HEIGHT : PANE_CTRL_HEIGHT)
+#define PANE_MSG_BOX_ID      2
+#define PANE_MSG_BOX_X       820 
+#define PANE_MSG_BOX_Y       750
+#define PANE_MSG_BOX_WIDTH   600
+#define PANE_MSG_BOX_HEIGHT  50 
 
+// world display pane center coordinates and zoom
+double       center_x = world::WORLD_WIDTH / 2;
+double       center_y = world::WORLD_HEIGHT / 2;
 const double ZOOM_FACTOR = 1.1892071;
 const double MAX_ZOOM    = 64.0 - .01;
 const double MIN_ZOOM    = (1.0 / ZOOM_FACTOR) + .01;
+double       zoom = 1.0; 
 
-double zoom = 1.0 / ZOOM_FACTOR;
-double center_x = world::WORLD_WIDTH / 2;
-double center_y = world::WORLD_HEIGHT / 2;
+// simulation cycle time
+const int TARGET_CYCLE_TIME_US = 50000;  // 50 ms
+
+// pane message box 
+const int MAX_MESSAGE_AGE = 200;
+string    message = "";
+int       message_age = MAX_MESSAGE_AGE;
+
+// create roads mode
+const int INITIAL_CREATE_ROAD_X = 2048;
+const int INITIAL_CREATE_ROAD_Y = 2048;
+const int INITIAL_CREATE_ROAD_DIR = 0;
+bool      create_roads_run = false;
+double    create_road_x = INITIAL_CREATE_ROAD_X;
+double    create_road_y = INITIAL_CREATE_ROAD_Y;
+double    create_road_dir = INITIAL_CREATE_ROAD_DIR;
+int       create_road_steering_idx = 5;
 
 // -----------------  MAIN  ------------------------------------------------------------------------
 
@@ -38,21 +66,14 @@ int main(int argc, char **argv)
 {
     enum mode { MAIN, CREATE_ROADS };
 
-    const int MAX_MESSAGE_AGE = 200;
-    const int DELAY_MICROSECS = 10000;
+    string    world_filename = "world.dat";
+    enum mode mode = MAIN;
+    bool      done = false;
+    long      start_time_us, end_time_us, delay_us;
 
-    string                world_filename = "world.dat";
-    enum mode             mode = MAIN;
-    struct display::event event = {display::EID_NONE,0,0};
-    bool                  done = false;
-    bool                  create_roads_run = false;
-    double                create_road_x = 2048;
-    double                create_road_y = 2048;
-    double                create_road_dir = 0;
-    int                   create_road_steering_idx = 5;
-    string                message = "";
-    int                   message_age = MAX_MESSAGE_AGE;
-    double                avg_cycle_time = DELAY_MICROSECS;
+    //
+    // INITIALIZATION
+    //
 
     // get options
     while (true) {
@@ -80,46 +101,47 @@ int main(int argc, char **argv)
     message = w.read_ok() ? "READ SUCCESS" : "READ FAILURE";
     message_age = 0;
 
-    // loop
-    while (!done) {
-        // determine average cycle time
-        // XXX make this a routine
-        {
-            const int   MAX_TIMES=10;
-            static long times[MAX_TIMES];
-            memmove(times+1, times, (MAX_TIMES-1)*sizeof(long));
-            times[0] = microsec_timer();
-            if (times[MAX_TIMES-1] != 0) {
-                avg_cycle_time = (times[0] - times[MAX_TIMES-1]) / (MAX_TIMES-1);
-            }
+    //
+    // MAIN LOOP
+    //
 
-            // xxx temp
-            static int count;
-            if ((++count % 100) == 0) {
-                INFO("AVG CYCLE TIME " << avg_cycle_time / 1000. << endl);
-            }
-        }
-            
+    while (!done) {
+        //
+        // STORE THE START TIME
+        //
+
+        start_time_us = microsec_timer();
+
+        //
+        // DISPLAY UPDATE 
+        // 
+
         // start display update
-        d.start(0, 0, PANE_WORLD_WIDTH, PANE_WORLD_HEIGHT,                                // pane 0: x,y,w,h
-                PANE_WORLD_WIDTH+PANE_SEPERATION, 0, PANE_CTRL_WIDTH, PANE_CTRL_HEIGHT);  // pane 1: x,y,w,h
+        d.start(PANE_WORLD_X,   PANE_WORLD_Y,   PANE_WORLD_WIDTH,   PANE_WORLD_HEIGHT, 
+                PANE_CTRL_X,    PANE_CTRL_Y,    PANE_CTRL_WIDTH,    PANE_CTRL_HEIGHT,
+                PANE_MSG_BOX_X, PANE_MSG_BOX_Y, PANE_MSG_BOX_WIDTH, PANE_MSG_BOX_HEIGHT);
 
         // draw world 
         w.place_car_init();
+        // xxx test w.place_car(2000, 2020, 0);
+        // xxx test w.place_car(2000, 2040, 180);
+        // xxx test w.place_car(2020, 2040, 90);
+        // xxx test w.place_car(2040, 2040, 270);
         if (mode == CREATE_ROADS) {
             w.place_car(create_road_x, create_road_y, create_road_dir);
         }
-        w.draw(0,center_x,center_y,zoom);
+        w.draw(PANE_WORLD_ID,center_x,center_y,zoom);
 
         // draw for mode CREATE_ROADS
         if (mode == CREATE_ROADS) {
             std::ostringstream s;
+            const double distance = 0.5;
 
-            // xxx comments
             if (create_roads_run) {
-                // xxx dont go off end of world
-                // xxx return distance
                 w.create_road_slice(create_road_x, create_road_y, create_road_dir);  
+
+                create_road_y += distance * sin((create_road_dir+270) * (M_PI/180.0));
+                create_road_x += distance * cos((create_road_dir+270) * (M_PI/180.0));
 
                 create_road_dir += (create_road_steering_idx - 5) * 0.05;
                 if (create_road_dir < 0) {
@@ -133,8 +155,8 @@ int main(int argc, char **argv)
 
             s.str("");
             if (create_roads_run) {
-                int mph = 0.5 / avg_cycle_time * (3600000000.0 / 5280.0);
-                s << "RUNNING - SPEED " << mph;
+                int mph = distance / TARGET_CYCLE_TIME_US * (3600000000.0 / 5280.0);
+                s << "RUNNING - " << mph << " MPH";
             } else {
                 s << "STOPPED";
             }
@@ -149,7 +171,7 @@ int main(int argc, char **argv)
 
         // draw the message box
         if (message_age < MAX_MESSAGE_AGE) {
-            d.text_draw(message, 17, 0, 1);
+            d.text_draw(message, 17, 0, PANE_MSG_BOX_ID);
             message_age++;
         }
 
@@ -159,10 +181,10 @@ int main(int argc, char **argv)
         int eid_1=-1, eid_9=-1, eid_run=-1, eid_stop=-1, eid_done=-1, eid_rol=-1, eid_ror=-1, eid_click=-1;
         __attribute__((unused)) int eid_2=-1, eid_3=-1, eid_4=-1, eid_5=-1, eid_6=-1, eid_7=-1, eid_8=-1;
 
-        eid_write    = d.text_draw("WRITE",        13,  0, 1, true);      
-        eid_quit     = d.text_draw("QUIT",         13, 10, 1, true);     
-        eid_reset    = d.text_draw("RESET",        15,  0, 1, true);     
-        eid_clear    = d.text_draw("CLEAR",        15, 10, 1, true);      
+        eid_write    = d.text_draw("WRITE",        13,  0, PANE_CTRL_ID, true);      
+        eid_quit     = d.text_draw("QUIT",         13, 10, PANE_CTRL_ID, true);     
+        eid_reset    = d.text_draw("RESET",        15,  0, PANE_CTRL_ID, true);     
+        eid_clear    = d.text_draw("CLEAR",        15, 10, PANE_CTRL_ID, true);      
         eid_quit_win = d.event_register(display::ET_QUIT);
         eid_pan      = d.event_register(display::ET_MOUSE_MOTION, 0);
         eid_zoom     = d.event_register(display::ET_MOUSE_WHEEL, 0);
@@ -192,8 +214,11 @@ int main(int argc, char **argv)
         // finish, updates the display
         d.finish();
 
-        // event handling
-        event = d.event_poll();
+        //
+        // EVENT HADNLING 
+        // 
+
+        struct display::event event = d.event_poll();
         do {
             // common events
             if (event.eid == display::EID_NONE) {
@@ -205,6 +230,9 @@ int main(int argc, char **argv)
             }
             if (event.eid == eid_clear) {
                 w.clear();
+                create_road_x = INITIAL_CREATE_ROAD_X;
+                create_road_y = INITIAL_CREATE_ROAD_Y;
+                create_road_dir = INITIAL_CREATE_ROAD_DIR;
                 break;
             }
             if (event.eid == eid_write) {
@@ -250,6 +278,7 @@ int main(int argc, char **argv)
                 }
                 if (event.eid == eid_stop) {
                     create_roads_run = false;
+                    create_road_dir = (int)(create_road_dir + 0.5);
                     break;
                 }
                 if (event.eid == eid_done) {
@@ -269,30 +298,58 @@ int main(int argc, char **argv)
                     create_road_y = (center_y - world_display_height / 2) + 
                                     (world_display_height / PANE_WORLD_HEIGHT * event.val2);
                     create_road_dir = 0;
-                    INFO("MOUSE " << event.val1 << " " << event.val2 << endl);
-                    INFO("XY = " << create_road_x << " " << create_road_y << endl);
+                    // xxx INFO("MOUSE " << event.val1 << " " << event.val2 << endl);
+                    // xxx INFO("XY = " << create_road_x << " " << create_road_y << endl);
                 }
                 if (event.eid == eid_rol) {
-                    create_road_dir--;
+                    create_road_dir = (int)(create_road_dir + 0.5) - 1;
                     if (create_road_dir < 0) {
                         create_road_dir += 360;
-                    } else if (create_road_dir > 360) {
-                        create_road_dir -= 360;
                     }
                 }
                 if (event.eid == eid_ror) {
-                    create_road_dir++;
-                    if (create_road_dir < 0) {
-                        create_road_dir += 360;
-                    } else if (create_road_dir > 360) {
+                    create_road_dir = (int)(create_road_dir + 0.5) + 1;
+                    if (create_road_dir > 360) {
                         create_road_dir -= 360;
                     }
                 }
             }
         } while (0);
 
-        // delay 
-        microsec_sleep(DELAY_MICROSECS);
+        //
+        // DELAY TO COMPLETE THE TARGET CYCLE TIME
+        //
+
+        // delay to complete TARGET_CYCLE_TIME_US
+        end_time_us = microsec_timer();
+        delay_us = TARGET_CYCLE_TIME_US - (end_time_us - start_time_us);
+        microsec_sleep(delay_us);
+
+        // oncer per second, debug print this cycle's processing tie
+        static int count;
+        if (++count == 1000000 / TARGET_CYCLE_TIME_US) {
+            count = 0;
+            INFO("PROCESSING TIME = " << end_time_us-start_time_us << " us" << endl);
+        }
+
+#if 1
+        // determine average cycle time
+        // xxx make this a routine, or just delete
+        {
+            const int   MAX_TIMES=10;
+            static long times[MAX_TIMES];
+            double avg_cycle_time = 0;
+            memmove(times+1, times, (MAX_TIMES-1)*sizeof(long));
+            times[0] = microsec_timer();
+            if (times[MAX_TIMES-1] != 0) {
+                avg_cycle_time = (times[0] - times[MAX_TIMES-1]) / (MAX_TIMES-1);
+            }
+            static int count;
+            if ((++count % 100) == 0) {
+                INFO("AVG CYCLE TIME " << avg_cycle_time / 1000. << endl);
+            }
+        }
+#endif
     }
 
     return 0;
